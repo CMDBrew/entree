56dd5294e512cd09b8dd4763db5a464b518f4081
Nelson Lee <nelson@cmdbrew.com>
2019-11-26 11:43:58 -0800

    Fix generators - add post generator - fix naming for tag and category generators

--- lib/jekyll_cmdbrew/post_generator.rb
+++ lib/jekyll_cmdbrew/post_generator.rb
@@ -4,7 +4,7 @@ module Jekyll
 
   module Paginate
 
-    module Tag
+    module Post
 
       # Paginiate collection
       class Pagination < Generator
@@ -13,26 +13,22 @@ module Jekyll
         priority :lowest
 
         def generate(site)
-          dir = site.config['tag_dir'] || 'tags'
+          folder = site.config['post_dir'] || 'posts'
 
-          site.tags.each do |tag, posts|
-            folder = File.join(dir, tag)
-            create_pages(site, posts, folder, tag)
-          end
+          create_pages(site, site.posts, folder)
         end
 
         private
 
-        def create_pages(site, posts, folder, tag)
+        def create_pages(site, posts, folder)
           if Paginate::Pager.pagination_enabled?(site)
             total =
               Paginate::Pager.calculate_pages(posts, site.config['paginate'])
             (1..total).each do |i|
-              site.pages <<
-                IndexPage.new(site, site.source, folder, tag, i)
+              site.pages << IndexPage.new(site, site.source, folder, i)
             end
           else
-            site.pages << IndexPage.new(site, site.source, folder, tag)
+            site.pages << IndexPage.new(site, site.source, folder)
           end
         end
 
@@ -42,12 +38,12 @@ module Jekyll
       class IndexPage < Page
 
         # rubocop:disable Metrics/MethodLength
-        def initialize(site, base, dir, tag, num_page = nil)
+        def initialize(site, base, dir, num_page = nil)
           @site = site
           @base = base
           @dir  = dir
           @name = html_filename(site, num_page, name: '')
-          layout = site.config['tag_layout'] || 'tags.html'
+          layout = site.config['post_layout'] || 'posts.html'
           # Path is only used by the convertible module and accessed below when calling read_yaml
           # Handling themes stored in a gem
           @path =
@@ -59,7 +55,7 @@ module Jekyll
 
           process(@name)
           read_yaml(File.join(@base, '_layouts'), layout)
-          assign_page_data(site, num_page, tag)
+          assign_page_data(site, num_page)
         end
         # rubocop:enable Metrics/MethodLength
 
@@ -78,14 +74,10 @@ module Jekyll
           name + 'index.html'
         end
 
-        def assign_page_data(site, num_page, tag)
-          if Paginate::Pager.pagination_enabled?(site)
-            data['paginator'] =
-              Paginate::Pager.new(site, num_page, site.tags[tag])
-          end
+        def assign_page_data(site, num_page)
+          return unless Paginate::Pager.pagination_enabled?(site)
 
-          data['title'] = tag
-          data['tag'] = tag
+          data['paginator'] = Paginate::Pager.new(site, num_page)
         end
 
       end
