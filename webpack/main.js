fa00daf79fe914b088609a83b8607448c9f51c4c
Nelson Lee <nelson@cmdbrew.com>
2019-12-06 13:10:49 -0800

    Integrate OwlCarousel2

--- webpack/main.js
+++ webpack/main.js
@@ -1,7 +1,13 @@
-// Import Plugins
+// Import components and bridge them
+import Carousel from './components/carousel';
+$.widget.bridge('appCarousel', Carousel);
+
+// Initialize Bootstrap addons
 window.Popper = Popper;
 
+// Initialize application js
 $(document).ready(function() {
+  $('[data-js="carousel"]').appCarousel()
   $('[data-toggle="popover"]').popover();
   $('[data-toggle="tooltip"]').tooltip();
 });
