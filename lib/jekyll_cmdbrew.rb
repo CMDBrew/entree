7425eb21e3d7daa5f1b3ae313288669003474ce5
Nelson Lee <nelson@cmdbrew.com>
2019-12-10 22:26:14 -0800

    Include byebug by default for development

--- lib/jekyll_cmdbrew.rb
+++ lib/jekyll_cmdbrew.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'byebug' if Jekyll.env.eql? 'development'
 require 'jekyll'
 require 'jekyll_cmdbrew/category_generator'
 require 'jekyll_cmdbrew/tag_generator'
